55f9172f6150f4799745f2926af78d781f26f9b7
javier cruz obispo <javier.cruz.odispo@hotmail.com>
2022-03-21 19:08:48 -0600

    uodate system and using directive

--- src/js/dbTodo.js
+++ src/js/dbTodo.js
@@ -19,7 +19,78 @@ let testing = ({ use }) => ({
 
 export default {
   install: (app, options) => {
-    app.config.globalProperties.db = options.use
+    const { use } = options
+    app.config.globalProperties.db = use
+    app.directive('db', (HTML, props) => {
+      if (HTML.parentNode.nodeName == 'FORM') {
+        const { dir, instance, modifiers, value } = props
+        let { show, hidden, max, btn, task } = modifiers
+        if ((show || hidden) && !(show & hidden)) {
+          const { id } = HTML.parentNode
+          const { auto } = modifiers
+          if (auto) {
+            if (instance.db.get(instance.db.length))
+              HTML.className = show ? 'show' : hidden ? 'hidden' : ''
+            else HTML.className = show ? 'hidden' : hidden ? 'show' : ''
+            id.addEventListener('change', ({ target }) => {
+              if (instance.db.get(target.value))
+                HTML.className = show ? 'show' : hidden ? 'hidden' : ''
+              else HTML.className = show ? 'hidden' : hidden ? 'show' : ''
+            })
+            instance.db.on('update', () => {
+              if (instance.db.get(id.value))
+                HTML.className = show ? 'show' : hidden ? 'hidden' : ''
+              else HTML.className = show ? 'hidden' : hidden ? 'show' : ''
+            })
+          } else HTML.className = show ? 'show' : hidden ? 'hidden' : ''
+        } else if (max && HTML.nodeName == 'INPUT') {
+          const { auto } = modifiers
+          if (auto) HTML.max = instance.db.length
+          else HTML.max = value
+        } else if (btn) {
+          const { id } = HTML.parentNode
+          const { auto } = modifiers
+          let v = ''
+          if (auto) v = HTML.name
+          else if (value != '') v = value
+          HTML.addEventListener('click', function () {
+            if (v == 'delete') {
+              instance.db.del(id.value)
+            } else if (v == 'clear') {
+              instance.db.clear()
+            } else instance.db.alert('not defined more btns')
+          })
+        } else if (task) {
+          const { id } = HTML.parentNode
+          const { onChange } = modifiers
+          if (onChange) {
+            id.addEventListener('change', ({ target }) => {
+              if (instance.db.get(target.value)) {
+                if (HTML.type == 'checkbox') {
+                  HTML.checked = instance.db.get(target.value)[HTML.name]
+                } else if (HTML.type == 'text') {
+                  HTML.value = instance.db.get(target.value)[HTML.name]
+                } else {
+                  instance.db.alert(
+                    'not defined accion in onchange task directive',
+                  )
+                }
+              } else {
+                if (HTML.type == 'checkbox') {
+                  HTML.checked = false
+                } else if (HTML.type == 'text') {
+                  HTML.value = ''
+                } else {
+                  instance.db.alert(
+                    'not defined accion in onchange task directive',
+                  )
+                }
+              }
+            })
+          }
+        }
+      } else instance.db.alert('require parent form')
+    })
     app.mixin(testing(options))
   },
 }
--- src/page/Home.vue
+++ src/page/Home.vue
@@ -1,26 +1,17 @@
 <template>
   <div>
     <h1>{{ title }}</h1>
-    <form action="#" @submit.prevent="submit" id="data">
-      <input type="number" name="id" min="0" @change="change"/>
-      <input type="text" name="title" placeholder="titulo"/>
-      <input type="text" name="description" placeholder="descripcion"/>
-      <input type="checkbox" name="state"/>
-      <input type="submit" value="guardar"/>
-      <input type="button" value="delete" :class="{ show: exist , hidden: !exist}" name="delete_btn" @click="click"/>
-      <input type="button" value="clear All" @click="clearAll">
+    <form action="#" @submit.prevent="saveTask" id="data">
+      <input type="number"    name="id"           v-db.max.auto min="0"  :value="this.db.length"  />
+      <input type="text"      name="title"        v-db.task.onChange placeholder="titulo" />
+      <input type="text"      name="description"  v-db.task.onChange placeholder="descripcion" />
+      <input type="checkbox"  name="state"        v-db.task.onChange/>
+      <input type="submit"    name="save"         value="guardar"/>
+      <input type="button"    name="delete"       v-db.show.auto value="delete"  v-db.btn.auto/>
+      <input type="button"    name="clear"        v-db.btn.auto value="clear All"  />
     </form>
   </div>  
 </template>
-
-<style scoped>
-  input[name="delete_btn"].show{
-    display:inline-block;
-  }
-  input[name="delete_btn"].hidden{
-    display:none;
-  }
-</style>
 
 <script>
 export default {
@@ -30,61 +21,40 @@ export default {
   },
   data(){
     return {
-      exist:false,
-      click({target:{parentNode}}){
-        this.dbdel(parentNode.querySelector("input[name='id']").value);
-      },
-      clearAll(){
-        this.db.clear()
-      },
-      change({target}){
-        const {title,description,state} = document.querySelector("form#data").children;
-        if(this.dbget(target.value)){
-          this.exist = true;
-          title.value = this.dbget(target.value).title
-          description.value = this.dbget(target.value).description
-          state.checked = this.dbget(target.value).state
-        }else{
-          this.exist = false;
-          title.value = ""
-          description.value = ""
-          state.checked = false
-        }
-      },
-      submit({target:{children}}){
-        const {id,title,description,state} = children;
-        let data = {
-          "title" : title.value,
-          "description" : description.value,
-          "state" : state.checked
-          };
-        if(data.title!=""||data.description!=""){
-          if(!!this.dbget(id.value)){
-            this.dbupdate(id.value,data);
-          }else{
-            this.dbpush(data);
-          }
-        }else{
-          this.db.alert("require content in title or decription")
-        }
-        title.value = "";
-        description.value = "";
-        state.checked = false;
-      }
+      exist:false
     }
   },
-  mounted(){
-    const {id} = document.querySelector("form#data").children;
-    id.max = this.db.length;
-    id.value = this.db.length;
-    this.db.on("update",_=>this.exist = !!this.dbget(id.value))
+  methods:{
+    saveTask({target:{children}}){
+      const {id,title,description,state} = children;
+      let data = {
+        "title" : title.value,
+        "description" : description.value,
+        "state" : state.checked
+        };
+      if(data.title!=""||data.description!=""){
+        if(!!this.dbget(id.value)){
+          this.dbupdate(id.value,data);
+        }else{
+          this.dbpush(data);
+        }
+      }else{
+        this.db.alert("require content in title or decription")
+      }
+      title.value = "";
+      description.value = "";
+      state.checked = false;
+    }
   },
   events:{
     update({target}){
-      const {id} = document.querySelector("form#data").children;
+      const {id,title,description,state} = document.querySelector("form#data").children;
       localStorage.setItem("items",JSON.stringify(target.db))
       id.max = this.db.length;
       id.value = this.db.length;
+      title.value = "";
+      description.value = "";
+      state.checked = false;
     },
     error:({detail:{msg}})=>{
       console.warn(msg);
