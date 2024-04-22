a881191d41970190bc7ac2be0026d2abc81eb626
Javier Cruz <31808656+thejavierCO@users.noreply.github.com>
2024-04-22 02:58:31 +0000

    reset proyect

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,18 @@
 import HomeV from '../page/Home.vue'
-import dbTodo from './dbTodo'
 import { createApp } from 'vue'
-import { Button } from 'vant';
-import { NavBar } from 'vant';
-import { Grid, GridItem } from 'vant';
-import { Form, Field, CellGroup } from 'vant';
-import { Stepper } from 'vant';
-import TodoApi from './TodoApp'
-import 'vant/lib/index.css';
 
-let app = createApp(HomeV, {
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+createApp(HomeV, {
   title: 'My app in working',
 })
-
-app.use(Button);
-app.use(NavBar);
-app.use(Grid);
-app.use(GridItem);
-app.use(Form);
-app.use(Field);
-app.use(CellGroup);
-app.use(Stepper);
-
-app.use(dbTodo, {
-  use: new TodoApi(
-    JSON.parse(localStorage.getItem('items')) ||
-    JSON.stringify(localStorage.setItem('items', '{}')),
-  ),
-})
-
-app.mount('#app')
+  .use(vuetify)
+  .mount('#app')
